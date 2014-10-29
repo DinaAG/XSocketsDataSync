57ba061bbe2933b8912d2efa4b2a15b45aa60b4a
XSockets (@ulfbjo) <codeplanner@gmail.com>
2014-10-29 22:34:49 +0100

    Added serilog config

--- src/DataSyncSample/DataSyncSample.Web/Startup.cs
+++ src/DataSyncSample/DataSyncSample.Web/Startup.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Serilog;
+using XSockets.Logger;
 using XSockets.Owin.Host;
 
 [assembly: OwinStartupAttribute(typeof(DataSyncSample.Web.Startup))]
 namespace DataSyncSample.Web
 {
+    public class MyLogger : XLogger
+    {
+        public MyLogger()
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Trace().MinimumLevel.Information().CreateLogger();
+        }
+    }
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
